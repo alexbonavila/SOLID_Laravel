07347ae8fb1df69264040aa1e7af835c7aece813
alexbonavila <alexbonavila@iesebre.com>
2016-01-18 17:23:21 +0100

    taula de seed feta 2

--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -28,10 +28,10 @@
                         <table class="table table-hover">
                             <tr>
                                 <th>ID</th>
-                                <th>User</th>
-                                <th>Date</th>
-                                <th>Status</th>
-                                <th>Reason</th>
+                                <th>Name</th>
+                                <th>Created at</th>
+                                <th>Updated at</th>
+                                <th>Total</th>
                             </tr>
                             <tr>
                                 <td>183</td>
@@ -40,27 +40,7 @@
                                 <td><span class="label label-success">Approved</span></td>
                                 <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                             </tr>
-                            <tr>
-                                <td>219</td>
-                                <td>Alexander Pierce</td>
-                                <td>11-7-2014</td>
-                                <td><span class="label label-warning">Pending</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                            </tr>
-                            <tr>
-                                <td>657</td>
-                                <td>Bob Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="label label-primary">Approved</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                            </tr>
-                            <tr>
-                                <td>175</td>
-                                <td>Mike Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="label label-danger">Denied</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                            </tr>
+
                         </table>
                     </div>
                     <!-- /.box-body -->
